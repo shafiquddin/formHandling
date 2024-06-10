7f1796645f01dd34fda1d2ceb880bb4388543e08
shafiquddin <shafiquddin2k@gmail.com>
2024-06-10 18:16:41 +0530

    stateLogin

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Input from "./Input";
+import { isEmail,isNotEmpty,hasMinLength } from '../util/validation.js'
 
 export default function StateLogin() {
     const [inputInvalid, setInputInvalid] = useState({
@@ -30,15 +31,15 @@ export default function StateLogin() {
         }));
     };
 
-    const emailIsValid = inputForm.email.includes('@');
-    const passwordIsValid = inputForm.password.trim().length >= 6;
+    const emailIsValid = inputInvalid.email && !isEmail(inputForm.email) && !isNotEmpty(inputForm.password)
+    const passwordIsValid =inputInvalid.password && !isNotEmpty(inputForm.password) && !hasMinLength(inputForm.password);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        if (!emailIsValid || !passwordIsValid) {
+        if (emailIsValid || passwordIsValid) {
             setInputInvalid({
-                email: !emailIsValid,
-                password: !passwordIsValid
+                email: emailIsValid,
+                password: passwordIsValid
             });
             return;
         }
@@ -53,11 +54,11 @@ export default function StateLogin() {
                 <Input label="Email" type="email" name="email" id="email" value={inputForm.email}
                     onBlur={() => onBlurHandler('email')}
                     onChange={(event) => onInputChangeHandler('email', event.target.value)}
-                    error={(inputInvalid.email && !emailIsValid) && 'Please enter a valid email'} />
+                    error={emailIsValid && 'Please enter a valid email'} />
                 <Input label="Password" type="password" name="password" id="password" value={inputForm.password}
                     onBlur={() => onBlurHandler('password')}
                     onChange={(event) => onInputChangeHandler('password', event.target.value)}
-                    error={(inputInvalid.password && !passwordIsValid) && 'Please enter a valid Password'} />
+                    error={passwordIsValid && 'Please enter a valid Password'} />
             </div>
 
             <p className="form-actions">
